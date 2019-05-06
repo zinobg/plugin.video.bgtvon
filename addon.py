b201379432b466b95b312cdc29e6dba8aaa5e257
Konstantin Yovchev <33600562+zinobg@users.noreply.github.com>
2019-05-06 14:38:20 +0200

    Update addon.py

--- addon.py
+++ addon.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-#     Copyright (C) 2018 inobg [at] gmail.com
+#     Copyright (C) 2018 zinobg [at] gmail.com
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
